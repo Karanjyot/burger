03f74178ca8bcdfe0e33b9c938b108f7c8812800
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-01-30 22:09:44 -0500

    main route set up

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const exphbs = require("express-handlebars");
 
 // console.log(sql);
 
+
+
+var app = express();
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 const mysql = require("mysql");
 
 var connection = mysql.createConnection({
@@ -19,3 +31,25 @@ var connection = mysql.createConnection({
     console.log("connected");
   });
 
+app.get("/", (req,res) =>{
+
+
+  connection.query("SELECT * FROM burgers", function(err, response){
+
+    if (err) throw err
+
+    console.log(response);
+
+    var data = {
+        burgersdata: [...response]}
+
+    res.render("index", data);
+
+  })
+
+    
+})
+
+  app.listen(PORT, ()=>{
+      console.log(`connected to PORT ${PORT}`);
+  })
