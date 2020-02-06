7557f7697b3c1c708f4d13b22a54e892bea12bd0
Karanjyot Thandi <karanjyot.thandi@gmail.com>
2020-02-05 19:12:51 -0500

    added jaws db connection

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express  = require("express");
 const exphbs = require("express-handlebars");
-// const sql = require("./config/connection.js");
 
-// console.log(sql);
+const mysql = require("mysql");
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'bootcamp123!',
+    database: 'burgers_db'
+  })
+};
 
 
 
@@ -18,15 +28,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-const mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "bootcamp123!",
-    database: "burgers_db"
-  });
+// var connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "bootcamp123!",
+//     database: "burgers_db"
+//   });
   
   connection.connect(function(err) {
     if (err) throw err;
